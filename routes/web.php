d438c1aea26fababc8591cdda98b7fc0a1fb6a17
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-11-27 22:20:29 +0400

    view all posts by an author

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+
+Route::get('/', function () {
+    return view('posts', [
+        'posts' => Post::latest()->get(),
+    ]);
+});
+
+Route::get('posts/{post}', function (Post $post) {
+    return view('post', ['post' => $post]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
+
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
+    ]);
+});
+
