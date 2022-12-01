f562d0bcb37e4ce9dd1649f347648aabb721a5a0
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-12-01 16:19:32 +0400

    search posts based on user request

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -27,14 +27,12 @@
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
-                <p>
-                    {{$post->excerpt}}
-                </p>
+            <div class="text-sm mt-4 space-y-4">
+                    {!! $post->excerpt !!}
             </div>
 
             <footer class="flex justify-between items-center mt-8">
-                <div class="flex items-center text-sm">
+                <div class="flex items-center text-sm ">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{$post->author->name}}</h5>
@@ -42,7 +40,7 @@
                     </div>
                 </div>
 
-                <div>
+                <div class="w-auto">
                     <a href="/posts/{{$post->slug}}"
                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                     >Read More</a>
