674f25d61b36ba4732d09384d416f1e474bea422
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-12-04 15:08:23 +0400

    add pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,11 +10,14 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
+            'posts' => Post::latest()->
+            filter(request(['search', 'category', 'author']))
+                ->paginate(6)->withQueryString(),
         ]);
     }
 
-    public function show(Post $post){
+    public function show(Post $post)
+    {
         return view('posts.show', ['post' => $post]);
     }
 
