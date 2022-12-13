4fda2bff114cf2d3882083f935623c18d5b2c627
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-12-13 19:43:24 +0400

    activate the comment form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,6 @@ class Post extends Model
     use HasFactory;
 
     protected $with = ['category', 'author'];
-    protected $guarded = [];
 
     public function scopeFilter($query, array $filters)
     {
