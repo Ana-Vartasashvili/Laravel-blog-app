def09b9a42d07b8f59c8f39f224f00457598c82d
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2022-12-19 22:13:59 +0400

    add a functionality to update posts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,6 +29,7 @@
                         <buttoon class="text-xs font-bold uppercase">Welcome, {{auth()->user()->name}}</buttoon>
                     </x-slot>
 
+                    <x-dropdown-item href="admin/posts" :active="request()->is('admin/posts')" >Dashboard</x-dropdown-item>
                     <x-dropdown-item href="admin/posts/create" :active="request()->is('admin/posts/create')" >New Post</x-dropdown-item>
                     <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()" >Log Out</x-dropdown-item>
 
